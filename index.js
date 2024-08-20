837e9bf1623c8be17077ce0850aeda3173504574
Nidhi Patel <nidhipatel@Nidhis-MacBook-Pro.local>
2024-08-20 15:07:38 -0400

    Authentication : Login Api

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express =  require('express');
+const app = express();
+const cors = require("cors");
+// const connection = require("./connect/db.js");
+const connectDB = require('./connect/db.js');
+const authRoutes = require('./routes/auth.js');
+
+
+
+//middlewares
+app.use(express.json())
+app.use(cors());
+
+//Database Connection
+connectDB();
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`listening on`, PORT)
+})
